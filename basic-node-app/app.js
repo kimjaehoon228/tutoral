e1fb09fdaa28f5d15107b8d09567fa322d547dfe
kimjaehoon228 <mario20222@gmail.com>
2023-12-17 23:23:39 +0900

    tmeplates/file and path
    서버 html 페이지 탬플릿 사용
    파일

--- basic-node-app/app.js
+++ basic-node-app/app.js
@@ -1,36 +1,20 @@
+const path = require('path');
 const express =require('express');
 const bodyParser = require('body-parser');
+
+const adminRouters = require('./router/admin');
+const homeRouters = require('./router/home');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended : false }));
 
-app.use((req,res,next) => {
- console.log('Logging the request')
- next();
+app.use('/users', adminRouters);
+app.use(homeRouters);
+
+app.use((req,res) => {
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
-
-
-app.get('/users', (req,res,next) => {
-    res.send('<h1>This is the users page');
-});
-
-app.get('/user/add', (req,res) => {
-    res.send(`<form method="POST">
-    <div><input name='username'/></div>
-    <div><button>add user</button></div>
-    </form>`);
-});
-
-app.post('/user/add',(req,res)=>{
-    console.log(req.body);
-    console.log('post request')
-    res.redirect('/');
-});
-
-app.get('/',(req,res,next) => {
-    res.send('<h1>This is the home page</h1>')
-});
-
 
 app.listen(3000,()=>{
     console.log('server started at port 3000');
