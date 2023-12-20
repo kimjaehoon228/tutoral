2288b0473145a45f76bf3beccbf18b7ca5c2a026
kimjaehoon228 <mario20222@gmail.com>
2023-12-21 00:26:48 +0900

    카트버튼 ejs design
    . Design Add to Cart button and add it in Product listing and details page

--- router/home.js
+++ router/home.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { getHomePage, getProductDetailsPage } = require('../controllers/HomeController');
+const { postCartPage, getCartPage } = require('../controllers/CartController');
 
 router.get('/', getHomePage);
-
 router.get('/product/details/:productId',getProductDetailsPage);
+
+router.post('/cart', postCartPage);
+router.post('/cart', getCartPage);
 
 module.exports = router;
